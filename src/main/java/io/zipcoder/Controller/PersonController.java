e4d75d41e6990a4fc05ccfc5245ecc39e94e234f
kanika-sharma-pro <kanikasharma1908@gmail.com>
2025-04-28 11:18:20 -0400

    completed person.java

---
+++ src/main/java/io/zipcoder/Controller/PersonController.java
@@ -0,0 +1,13 @@
+package io.zipcoder.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PersonController {
+
+
+    @RequestMapping("/people")
+
+}
---
+++ src/main/java/io/zipcoder/Person.java
@@ -0,0 +1,50 @@
+package io.zipcoder;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int idNum;
+public Person(String firstName, String lastName, int idNum) {
+this.firstName = firstName;
+this.lastName = lastName;
+this.idNum = idNum;
+}
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getIdNum() {
+        return idNum;
+    }
+
+    public void setIdNum(int idNum) {
+        this.idNum = idNum;
+    }
+
+
+public Person(){
+
+}
+
+}
